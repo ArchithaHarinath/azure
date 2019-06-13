01d8784cc218b2e940c393ff4c24f6578bfc6d12
ArchithaHarinath <archithaharinath@gmail.com>
2019-06-12 23:31:10 -0500

    sq

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, render_template
+from flask import Flask , render_template , request
 app = Flask(__name__)
 
 @app.route('/')
-def my_form():
-    return render_template('my-form.html')
+def home():
+	return "Hello world"
 	
 if __name__ == '__main__':
   app.run()
