2f4d243dcd3d4bdd72c9df27336134bd1b41621e
Architha <archithaharinath@gmail.com>
2019-06-14 14:18:44 -0500

    k

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from flask import *
 import pandas as pd
 application=app = Flask(__name__)
 import sqlite3 as sql
+import redis
+import time
+import _pickle as pickle
+
+myHostname = "architha.redis.cache.windows.net"
+myPassword = "0D0U2S4PfqMI7vkbaQ82TUNJB2jSYW2xpWawpgFPrHk="
+
+r = redis.StrictRedis(host=myHostname,port=6380, db=0, password=myPassword, ssl=True)
+
 
 @app.route('/')
 def home():
@@ -13,6 +22,7 @@ def upload_csv():
  
 @app.route('/addrec',methods = ['POST', 'GET'])
 def addrec():
+
 	if request.method == 'POST':
 		 con = sql.connect("database.db")
 		 csv = request.files['myfile']
@@ -23,12 +33,14 @@ def addrec():
 
 @app.route('/list')
 def list():
+	start_t=time.time()
 	con = sql.connect("database.db") 
 	cur = con.cursor()
 	cur.execute("select * from Earthquake")
 	rows = cur.fetchall();
 	con.close()
-	return render_template("list.html",rows = rows)
+	end_t=time.time()-start_t
+	return render_template("list.html",rows = rows,e=end_t)
 	
 if __name__ == '__main__':
 	app.run()
