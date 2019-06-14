1050c98dccf87facb7429718d61c3d2401b6ab3f
ArchithaHarinath <archithaharinath@gmail.com>
2019-06-13 22:47:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,31 +7,7 @@ import pandas as pd
 
 @app.route('/')
 def home():
-	return render_template('home.html')
+	return "hello world"
 
-
-@app.route('/enternew')
-def upload_csv():
-	return render_template('upload.html')
- 
-@app.route('/addrec',methods = ['POST', 'GET'])
-def addrec():
-	if request.method == 'POST':
-		 con = sql.connect("database.db")
-		 csv = request.files['myfile']
-		 file = pd.read_csv(csv)
-		 file.to_sql('Earthquake', con, schema=None, if_exists='replace', index=True, index_label=None, chunksize=None, dtype=None)	  
-		 con.close()
-		 return render_template("result.html",msg = "Record inserted successfully")
-
-@app.route('/list')
-def list():
-	con = sql.connect("database.db") 
-	cur = con.cursor()
-	cur.execute("select * from Earthquake")
-	rows = cur.fetchall();
-	con.close()
-	return render_template("list.html",rows = rows)
-	 
 if __name__ == '__main__':
 	app.run()
