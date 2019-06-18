4ab129dc4867533db0b6ae369926663cce224f08
Architha <archithaharinath@gmail.com>
2019-06-18 13:26:52 -0500

    l

--- main.py
+++ main.py
@@ -34,19 +34,23 @@ def upload_csv():
 def addrec():
 
 	if request.method == 'POST':
-		 con = sql.connect("database.db")
-		 csv = request.files['myfile']
-		 file = pd.read_csv(csv)
-		 file.to_sql('Earthquake', con, schema=None, if_exists='replace', index=True, index_label=None, chunksize=None, dtype=None)	  
-		 con.close()
-		 return render_template("result.html",msg = "Record inserted successfully")
+		con = sql.connect("database.db")
+		csv = request.files['myfile']
+		file = pd.read_csv(csv)
+		file.to_sql('Earthquake', con, schema=None, if_exists='replace', index=True, index_label=None, chunksize=None, dtype=None)	  
+		con.close()
+	return render_template("result.html",msg = "Record inserted successfully")
 
 @app.route('/list')
 def list():
 	
 	cache="mycache"
 	start_t = time.time()
-	query="select * from Earthquake"
+	query="select * from Earthquake "
+	
+	
+	#query="select County from Countries where state=(select FULL from Statef where CODE='AK')"
+	
 	if r.exists(cache):
 		t="with"
 		print(t)
@@ -59,59 +63,67 @@ def list():
 	else:
 		t="without"
 		print(t)
+	
 		con = sql.connect("database.db") 
 		cur = con.cursor()
 		cur.execute(query)
-		rows = cur.fetchall();
+		rows = str(cur.fetchall());
 		con.close()
+	
 		r.set(cache,pickle.dumps(rows))
 		end_t=time.time()-start_t
-		
-	return render_template("list.html",rows = rows,e=end_t, t=t)
+	
+	#print(lis)
+	return render_template("list.html",data = rows)
+	#return render_template("list.html",rows = rows,e=end_t, t=t),data = rows
 
-@app.route('/c_i')
-def c_i():
-	return render_template('c_i.html')
+@app.route('/magrange_i')
+def magrange_i():
+	return render_template('mag_i.html')
 	
 @app.route('/mag_list' , methods=['GET', 'POST'])
 def mag_list():
-	res=[]
-	cache="mycache"
-	cache_t=0
-	uncache_t=0
-	cc=0
-	uc=0
-	w_o=[]
-	st=time.time()
-	for i in range(1000):
-		ran_num="{:.2f}".format(random.uniform(-2,8))
-		#print (ran_num)
-		if r.exists(cache+str(i)):
-			#t="with"
-			start_t = time.time()
-			#rows = pickle.loads(r.get(cache+str(ran_num)))
-			rows=r.get(cache+str(i))
-			end_t=time.time()-start_t
-			cache_t+=end_t
-			cc+=1
-			#w_o.append(t)
-		else:	
-			start_t = time.time()
-			query = "select * from Earthquake where mag>" + str(ran_num)
-			t="without"
-			w_o.append(t)
-			con = sql.connect("database.db") 
-			cur = con.cursor()
-			cur.execute(query)
-			#rows = cur.fetchall()
-			end_t=time.time()-start_t
-			uncache_t+=end_t
-			#r.set(cache+str(ran_num),pickle.dumps(rows))
-			r.set(cache+str(i),1)
-			uc+=1
-			con.close()
-	#print(w_o)		
-	et=time.time()-st	
+	
+	if request.method=='POST':
+		mag_v1 = int(request.form["val1"])
+		mag_v2 = int(request.form["val2"])
+		res=[]
+		cache="mycache"
+		cache_t=0
+		uncache_t=0
+		cc=0
+		uc=0
+		w_o=[]
+		st=time.time()
+		for i in range(1000):
+			ran_num="{:.2f}".format(random.uniform(mag_v1,mag_v2))
+			#print (ran_num)
+			if r.exists(cache+str(i)):
+				#t="with"
+				start_t = time.time()
+				#rows = pickle.loads(r.get(cache+str(ran_num)))
+				rows=r.get(cache+str(i))
+				end_t=time.time()-start_t
+				cache_t+=end_t
+				cc+=1
+				#w_o.append(t)
+			else:	
+				start_t = time.time()
+				query = "select * from Earthquake where mag>" + str(ran_num)
+				t="without"
+				w_o.append(t)
+				con = sql.connect("database.db") 
+				cur = con.cursor()
+				cur.execute(query)
+				#rows = cur.fetchall()
+				end_t=time.time()-start_t
+				uncache_t+=end_t
+				#r.set(cache+str(ran_num),pickle.dumps(rows))
+				r.set(cache+str(i),1)
+				uc+=1
+				con.close()
+		#print(w_o)		
+		et=time.time()-st	
 	return render_template("mag_greater.html",un=uncache_t,c=cache_t,e=et)	
 	
 @app.route('/delete_i')
@@ -136,53 +148,56 @@ def deletion():
 	con.close()
 			
 	end_t=time.time()-start_t	
-	return render_template("d_o.html",data = w_o ,e=end_t)	
+	return render_template("d_o.html",data=rows,e=end_t)	
 
 @app.route('/names_in')
 def names_in():
 	return render_template('n_i.html')	
 
-@app.route('/names_out')
+@app.route('/names_out',methods=['GET', 'POST'])
 def names_out():
 	
-	cache="mycache"
-	query="select distinct net from Earthquake where net like 'n%' "
-	con = sql.connect("database.db") 
-	cur = con.cursor()
-	cur.execute(query)
-	rows =cur.fetchall();
-	con.close()
+	if request.method=='POST':
 	
-	start_t = time.time()
-	
-	for i in range(100):
-	
-		ran_num=random.randint(0,len(rows)-1)
-		#print (ran_num)
-		if r.exists(cache+str(rows[ran_num])):
-			t="with"
-			temp_res = pickle.loads(r.get(cache+str(rows[ran_num])))
-			#w_o.append(t)
-			
-		else:	
-			v=str(rows[ran_num])
-			#print(type(v[2:4]))
-			query = "select * from Earthquake where net = '"+v[2:4]+"'"       
-			print(query)
-			t="without"
-			#w_o.append(t)
-			con = sql.connect("database.db") 
-			cur = con.cursor()
-			cur.execute(query)
-			temp_res = cur.fetchall()
-			#if rows!= None:
-				#res.append(rows)
-			r.set(cache+str(rows[ran_num]),pickle.dumps(temp_res))
-			con.close()
-	#r.set(cache,pickle.dumps(rows))
-	end_t=time.time()-start_t
+		cache="mycache"
+		chara = request.form["val1"]
+		query="select distinct net from Earthquake where net like '"+chara+"%' "
+		con = sql.connect("database.db") 
+		cur = con.cursor()
+		cur.execute(query)
+		rows =cur.fetchall();
+		con.close()
 		
-	return render_template("n_o.html",e=end_t)
+		start_t = time.time()
+		
+		for i in range(100):
+		
+			ran_num=random.randint(0,len(rows)-1)
+			#print (ran_num)
+			if r.exists(cache+str(rows[ran_num])):
+				t="with"
+				temp_res = pickle.loads(r.get(cache+str(rows[ran_num])))
+				#w_o.append(t)
+				
+			else:	
+				v=str(rows[ran_num])
+				#print(type(v[2:4]))
+				query = "select * from Earthquake where net = '"+v[2:4]+"'"       
+				print(query)
+				t="without"
+				#w_o.append(t)
+				con = sql.connect("database.db") 
+				cur = con.cursor()
+				cur.execute(query)
+				temp_res = cur.fetchall()
+				#if rows!= None:
+					#res.append(rows)
+				r.set(cache+str(rows[ran_num]),pickle.dumps(temp_res))
+				con.close()
+		#r.set(cache,pickle.dumps(rows))
+		end_t=time.time()-start_t
+			
+		return render_template("n_o.html",e=end_t)
 	
 @app.route('/lat_i')
 def lat_i():
@@ -195,13 +210,6 @@ def select_lat():
 	rows = []
 	magnitudes=[]
 	if request.method=='POST':
-
-		query = " SELECT * FROM  Earthquake "
-		con = sql.connect("database.db") 
-		cur = con.cursor()
-		cur.execute(query)
-		rows =cur.fetchall();
-		con.close()
 		radius = 6373.0
 		res=[]
 		start_t = time.time()
@@ -212,18 +220,26 @@ def select_lat():
 			t="with"
 			temp_res = pickle.loads(r.get(cache+str(rows[ran_num])))
 		
-		for row in rows:
-			lat1 = radians(float(row[2]))
-			lon1 = radians(float(row[3]))
-			dist_lat = lat1-radians(float(request.form['val1']))
-			dist_lon = lon1 -radians(float(request.form['val2']))
-			formula =abs( sin(dist_lat / 2)*2 + cos(radians(float(request.form['val1']))) * cos(lat1) * sin(dist_lon / 2)*2)
-			ans = 2*atan2(sqrt(formula), 1-sqrt(formula))
-			distance = float(radius*ans)
+		else:
 			
-			val1=request.form['val3']
-			if distance <= (float(val1)):
-				res.append(row[5])
+			query = " SELECT * FROM  Earthquake "
+			con = sql.connect("database.db") 
+			cur = con.cursor()
+			cur.execute(query)
+			#rows =cur.fetchall()
+			con.close()
+			for row in rows:
+				lat1 = radians(float(row[2]))
+				lon1 = radians(float(row[3]))
+				dist_lat = lat1-radians(float(request.form['val1']))
+				dist_lon = lon1 -radians(float(request.form['val2']))
+				formula =abs( sin(dist_lat / 2)*2 + cos(radians(float(request.form['val1']))) * cos(lat1) * sin(dist_lon / 2)*2)
+				ans = 2*atan2(sqrt(formula), 1-sqrt(formula))
+				distance = float(radius*ans)
+				
+				val1=request.form['val3']
+				if distance <= (float(val1)):
+					res.append(row[5])
 		end_t=time.time()
 	return render_template("lat_out.html", end=end_t-start_t)
 
